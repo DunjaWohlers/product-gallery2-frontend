27d6be2401d6c4315d60add0354bbc42a0752d63
Steppzilla <45403455+Steppzilla@users.noreply.github.com>
2022-09-22 17:29:27 +0200

    add proxy to backend 8080 , add input-field for file-input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {FormEvent} from 'react';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
+export default function App() {
 
-    </div>
-  );
+    const handleSave = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const htmlFormElement = event.target as HTMLFormElement;
+        const formData = new FormData(htmlFormElement);
+
+        return axios.post("/api/uploadPicture", formData,
+        ).then(data => data.data)
+            .catch(() => {
+                    console.log("Bild konnte nicht gespeichert werden");
+                    return [];
+                }
+            );
+    }
+
+    return (
+        <div className="App">
+            <form onSubmit={handleSave}>
+                <label> Neues Bild hinzufügen: </label>
+                <input type={"file"} name={"file"}/>
+                <button type={"submit"}> speichern</button>
+            </form>
+        </div>
+    );
 }
-
-export default App;
